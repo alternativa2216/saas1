84dc469099c2188c9a0855c82eabb34d6b2d37b0
David Rodrigues Ferreira de Oliveira <david.oliveira@monetare.com.br>
2024-09-20 09:25:59 -0300

    liberdade

---
+++ frontend/build/precache-manifest.669368de57d2d54610c0da273bc71ba1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f38bba87a1187684f0aba230e39e68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6542b7ae07c1cdb7ea41",
+    "url": "/static/css/2.c6d31ef2.chunk.css"
+  },
+  {
+    "revision": "3a4e16fc962ac5e94eff",
+    "url": "/static/css/main.5b027334.chunk.css"
+  },
+  {
+    "revision": "6542b7ae07c1cdb7ea41",
+    "url": "/static/js/2.2e8d8136.chunk.js"
+  },
+  {
+    "revision": "dff54f53680a5e8b9fb3b103249371af",
+    "url": "/static/js/2.2e8d8136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e5332fd31c5d66ad121",
+    "url": "/static/js/3.1fb155c1.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.1fb155c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4e16fc962ac5e94eff",
+    "url": "/static/js/main.6b8e4bda.chunk.js"
+  },
+  {
+    "revision": "7e34263a490a021c871c",
+    "url": "/static/js/runtime-main.d9c43d9f.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "37517e607bd79239acbfae718f323794",
+    "url": "/static/media/logo.37517e60.png"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
